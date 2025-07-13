fa110a4e11b8e378817e982f46ac8ed834148d47
Adrian Cosio <adriancosio14@gmail.com>
2025-07-13 08:54:16 -0500

    Remove variables, insert methods inside print statement

--- main.py
+++ main.py
@@ -4,22 +4,13 @@ from user import User
 
 James = User("James", 24, "male", 6, 180, 36, 40)
 
-
 waist_to_hip_ratio = James.calculate_waist_to_hip_ratio()
-
-bmi = James.calculate_bmi_imperial()
-
-bf_percentage = James.calculate_body_fat_percentage(waist_to_hip_ratio)
-
-maintance_calories = James.calculate_maintance_calories()
-
-
 
 print(f"{James.name} is {James.age} years old")
 print("His body metrics are:")
 print(f"WHR (waist-to-hip ratio): {waist_to_hip_ratio}")
-print(f"BMI (body-mass-index): {bmi}")
-print(f"Body fat %: {bf_percentage}")
+print(f"BMI (body-mass-index): {James.calculate_bmi_imperial()}")
+print(f"Body fat %: {James.calculate_body_fat_percentage(waist_to_hip_ratio)}")
 
-print(f"Estimated maintance calories: {maintance_calories}")
+print(f"Estimated maintance calories: {James.calculate_maintance_calories()}")
 
