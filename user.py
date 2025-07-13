20f470a38d234ced58c54145fe11a158df29cca1
Adrian Cosio <adriancosio14@gmail.com>
2025-07-13 09:23:25 -0500

    change keys in activity_factor to lower case, change 'is' to '=='

--- user.py
+++ user.py
@@ -28,10 +28,10 @@ class User:
     def calculate_body_fat_percentage(self, waist_hip_ratio):
         # simplified Navy method
 
-        if self.gender is "male":
+        if self.gender == "male":
             body_fat_percentage = 76.76 * waist_hip_ratio - 49.82
 
-        if self.gender is "female":
+        if self.gender == "female":
             body_fat_percentage = 70.41 * waist_hip_ratio - 58.25
 
         return body_fat_percentage
@@ -43,17 +43,17 @@ class User:
         height_cm = self.height_ft * 0.393701     #inches to cm (To-Do: Convert 5'9" to inches first)
 
         # Basal Matabolic Rate
-        if self.gender is "male":
+        if self.gender == "male":
             bmr = (10 * weight_kg) + (6.25 * height_cm) - (5 * self.age) + 5
-        if self.gender is "female":
+        if self.gender == "female":
             bmr = (10 * weight_kg) + (6.25 * height_cm) - (5 * self.age) - 161
         
         activity_factor = {
-            'Sedantary': 1.2,           #little to no exercise
-            'Lightly Active': 1.375,    # light exercise/sports 1-3 days/week
-            'Moderate Active': 1.55,    # moderate exercise/sports 3-5 days/week
-            'Very Active': 1.725,       # hard exercise/sports 6-7 days a week
-            'Extra Active': 1.9,        # very hard exercise/sports & physical job
+            'sedantary': 1.2,           #little to no exercise
+            'lightly active': 1.375,    # light exercise/sports 1-3 days/week
+            'moderate active': 1.55,    # moderate exercise/sports 3-5 days/week
+            'very active': 1.725,       # hard exercise/sports 6-7 days a week
+            'extra active': 1.9,        # very hard exercise/sports & physical job
         }
 
         activity_factor_value = activity_factor.get(self.activity_level.lower())
