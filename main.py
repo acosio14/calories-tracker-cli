26de81fedb558ba5ea677a737bb1e7584034fd97
Adrian Cosio <adriancosio14@gmail.com>
2025-07-13 08:32:25 -0500

    Creata dataclass objects

---
+++ main.py
@@ -0,0 +1,9 @@
+from user import User
+
+James = User("James", 24, "male", 6, 180)
+Andy = User("Andy", 22, "male", 6, 190)
+Leslie = User("Leslie", 30, "female", 5, 125)
+Samantha = User("Samanta", 28, "female", 5, 100)
+
+
+
