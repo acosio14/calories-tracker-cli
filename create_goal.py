d5be463e5d7bed0a1c6daeddc6add6354a27696f
Adrian Cosio <adriancosio14@gmail.com>
2025-07-15 21:08:12 -0500

    Change calories_delta to delta

--- create_goal.py
+++ create_goal.py
@@ -4,13 +4,13 @@ def create_goal_plan(goal: str, tracking: bool = True) -> int:
     maintance = User.calculate_maintance_calories()
     
     if not tracking:
-        calories_delta = 500
+        delta = 500
     else:
         ...
         # linear regression of table, data, stored values.
 
     goals = { 'gain': 1, 'lose': -1, 'maintain': 0}
     
-    calories = maintance + ( goals.get(goal) * calories_delta )
+    calories = maintance + ( goals.get(goal) * delta )
     
     return calories
