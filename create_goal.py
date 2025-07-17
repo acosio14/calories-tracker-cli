c74c4096ea46d7011d0e0e46f83391afe94f4aa4
Adrian Cosio <adriancosio14@gmail.com>
2025-07-16 21:35:45 -0500

    Modify variable names

--- create_goal.py
+++ create_goal.py
@@ -1,19 +1,20 @@
 from user import User
 
 def create_goal_plan(goal: str, tracking: bool = True) -> int:
-    
+    # Currently only able to recalibrate to weight of 1
+
     delta = 500
-    weight_delta = 1
-    current_maintenance = 2500
+    current_weight_delta = 1
+    current_calories = 2500
+
+    weight_delta = { 'lose': -1, 'maintain': 0, 'gain': 1 }
 
     maintenance = User.calculate_maintance_calories()
     
     if tracking:
-        maintenance = current_maintenance - (500 * weight_delta)
-
-    goals = { 'gain': 1, 'lose': -1, 'maintain': 0}
+        maintenance = current_calories - (500 * current_weight_delta)
     
-    calories = maintenance + ( goals.get(goal) * delta )
+    calories = maintenance + ( weight_delta.get(goal) * delta )
     
     return calories
 
