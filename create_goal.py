8db7715e3cb88ef90d1acf672172f3929680cadf
Adrian Cosio <adriancosio14@gmail.com>
2025-07-19 18:30:08 -0500

    Simplify make and make readable calculate_recommended_calories fcn.

--- create_goal.py
+++ create_goal.py
@@ -5,24 +5,23 @@ from dataclasses import dataclass
 class HealthPlan:
     goal: str = 'lose' # lose, gain, maintain weight
     diet_type: str = 'balanced'
-    current_goal_calories: int = 1500, m = 2000 # Calories to lose, gain (subtract by 1 lb = 500 cal)
+    current_goal_calories: int = 1500 # Calories to lose, gain (subtract by 1 lb = 500 cal)
     current_weight_delta: float | int = - 1 # Default of lose 1 lb/week
     desired_weight_delta: float | int = - 1
 
 
     def calculate_recommended_calories(
             self,
-            my_user: UserProfile,
-            goal: str, 
+            my_user: UserProfile, 
             tracking: bool = False
     ) -> int:
         
-        maintenance_calories = my_user.calculate_maintance_calories()
-        
         if tracking:
             maintenance_calories = self.current_goal_calories - (500 * self.current_weight_delta)
+        else:
+            maintenance_calories = my_user.calculate_maintance_calories()
         
-        return maintenance_calories + ( self.desired_weight_delta * 500 )
+        return maintenance_calories + ( self.desired_weight_delta * 500 ) #recommended calories
 
 def main():
     ...
