e635f5779c640fe145f3b4dc95765b7f866b8920
Adrian Cosio <adriancosio14@gmail.com>
2025-08-02 18:54:44 -0500

    Make create_sql_table() and make weight_entries to test.

--- store_weight.py
+++ store_weight.py
@@ -2,21 +2,22 @@
 
 import sqlite3
 
-database = 'weight.db'
-create_table_sql = """
-CREATE TABLE IF NOT EXISTS weight_table (
-    id TEXT PRIMARY KEY,
-    date TEXT NOT NULL, 
-    weight REAL NOT NULL-- Stores weight
-);
-"""
+def create_sql_table():
+    database = 'weight.db'
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS weight_table (
+        id TEXT PRIMARY KEY,
+        date TEXT NOT NULL, 
+        weight REAL NOT NULL-- Stores weight
+    );
+    """
 
-with sqlite3.connect(database) as connection:
-    cursor = connection.cursor()
-    cursor.execute(create_table_sql)
-    connection.commit()
+    with sqlite3.connect(database) as connection:
+        cursor = connection.cursor()
+        cursor.execute(create_table_sql)
+        connection.commit()
 
-def add_weight(conn, weight_entry):
+def add_weight_entry(conn, weight_entry):
     insert_statement = ''' INSERT INTO weight_table(id, date, weight)
                             VALUES(?,?,?,?) '''
     
@@ -27,3 +28,14 @@ def add_weight(conn, weight_entry):
     conn.commit()
 
     return cursor.lastrowid
+
+weight_entries = [
+    (0, "2025-08-01", 180.0),
+    (0, "2025-08-02", 180.0),
+    (0, "2025-08-03", 180.0),
+    (0, "2025-08-04", 180.0),
+    (0, "2025-08-05", 180.0)
+]
+
+for weight_entry in weight_entries:
+    add_weight_entry(weight_entry)
