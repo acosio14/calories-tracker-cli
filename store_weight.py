cb6a20d8d797796b191e9a1440138d28e8b0bb36
Adrian Cosio <adriancosio14@gmail.com>
2025-08-02 18:47:29 -0500

    Add add_weight function to insert into database table.

--- store_weight.py
+++ store_weight.py
@@ -4,7 +4,7 @@ import sqlite3
 
 database = 'weight.db'
 create_table_sql = """
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS weight_table (
     id TEXT PRIMARY KEY,
     date TEXT NOT NULL, 
     weight REAL NOT NULL-- Stores weight
@@ -15,3 +15,15 @@ with sqlite3.connect(database) as connection:
     cursor = connection.cursor()
     cursor.execute(create_table_sql)
     connection.commit()
+
+def add_weight(conn, weight_entry):
+    insert_statement = ''' INSERT INTO weight_table(id, date, weight)
+                            VALUES(?,?,?,?) '''
+    
+    cursor = conn.cursor()
+
+    cursor.execute(insert_statement, weight_entry)
+
+    conn.commit()
+
+    return cursor.lastrowid
