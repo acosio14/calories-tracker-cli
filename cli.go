6eb54377d7f56f756f7258b54539f4b75d610c14
Adrian Cosio <adriancosio14@gmail.com>
2026-05-08 08:01:49 -0500

    feat: add placeholder functions to cli

--- cli.go
+++ cli.go
@@ -1,1 +1,38 @@
 package main
+
+type CLI struct{}
+
+func (c *CLI) CreateUser() error {
+	// probably add init weight
+	return nil
+}
+
+func (c *CLI) SelectUser() error {
+	return nil
+}
+
+func (c *CLI) SetCaloriesGoal() error {
+	// manually set calories
+	return nil
+}
+
+func (c *CLI) AddWeight() error {
+	return nil
+}
+
+func (c *CLI) AddFoodItem() error {
+	return nil
+}
+
+func (c *CLI) ViewRemaindingCalories() error {
+	return nil
+}
+
+func (c *CLI) DisplayWeightProgress() error {
+	return nil
+}
+
+func (c *CLI) UpdateCaloriesGoal() error {
+	// manually update calories
+	return nil
+}
