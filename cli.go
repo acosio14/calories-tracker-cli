029b5405fcf57c3f08ead4ff97e9e5a026e4cb4d
Adrian Cosio <adriancosio14@gmail.com>
2026-05-11 05:56:09 -0500

    feat: add user, add, edit and view command func placeholders.

--- cli.go
+++ cli.go
@@ -1,3 +1,46 @@
 package main
 
-type CLI struct{}
+import "github.com/spf13/cobra"
+
+func NewRootCmd() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:   "calorie-tracker",
+		Short: "Tracks Calories",
+	}
+	rootCmd.AddCommand(User())
+	rootCmd.AddCommand(Add())
+	rootCmd.AddCommand(Edit())
+	rootCmd.AddCommand(View())
+
+	return rootCmd
+}
+
+func User() *cobra.Command {
+	//create
+	//select
+	userCmd := &cobra.Command{
+		Use:   "User",
+		Short: "Creater or Select user.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+	userCmd.Flags().AddFlag()
+	return userCmd
+}
+
+func Add() *cobra.Command {
+	//Add: Goal, Calories, Weight, or Food item
+	return nil
+}
+
+func Edit() *cobra.Command {
+	//Edit: Goal or Food Item
+	return nil
+}
+
+func View() *cobra.Command {
+	//View: Remaining calories in the day
+	// Weight Progress (Plot)
+	return nil
+}
