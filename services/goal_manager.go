0d40ff1ee3b5f7a2eae2275a22872cb03b7d484f
Adrian Cosio <adriancosio14@gmail.com>
2026-05-16 17:01:48 -0500

    feat: add service files with placeholder functions.

---
+++ services/goal_manager.go
@@ -0,0 +1,11 @@
+package service
+
+type GoalManager struct{}
+
+func (g *GoalManager) AddCaloriesGoal() error {
+	return nil
+}
+
+func (g *GoalManager) EditCaloriesGoal() error {
+	return nil
+}
---
+++ services/user_manager.go
@@ -0,0 +1,11 @@
+package service
+
+type UserManager struct{}
+
+func (u *UserManager) CreateUser() error {
+	return nil
+}
+
+func (u *UserManager) SelectUser() error {
+	return nil
+}
