40f417e7176b0f8cbe216f46ff5e37eb086b1ec4
Adrian Cosio <adriancosio14@gmail.com>
2026-05-26 05:28:28 -0500

    feat: append a weight entry to user in AddWeight.

--- service/weight.go
+++ service/weight.go
@@ -1,14 +1,42 @@
 package service
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/acosio14/calories-tracker-cli/domain"
+)
+
 type WeightTracker struct{}
 
-func (w *WeightTracker) AddWeight() error {
+func (w *WeightTracker) AddWeight(weight float32) error {
+	// Appends a weight entry to the user's weight list
 	user, err := SelectUser()
 	if err != nil {
 		return err
 	}
 
-	//Now add weight to user domain
+	weight_entry := domain.Weight{
+		Date:  time.Now().Format("January 2, 2006"),
+		Value: weight,
+	}
+	user.Weight = append(user.Weight, weight_entry)
+
+	userData, err := json.MarshalIndent(user, "", "	")
+	if err != nil {
+		return err
+	}
+	filename := fmt.Sprintf("%s.json", user.Name)
+	outputPath := filepath.Join("output", filename)
+	err = os.WriteFile(outputPath, userData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
 
